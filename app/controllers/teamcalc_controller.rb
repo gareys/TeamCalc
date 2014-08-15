43556f9e97cca24a25d9ce8338d9b6e8ae13299b
Garey Simpson <webdesignator@gmail.com>
2014-08-15 17:31:22 -0400

    Solved

--- app/controllers/teamcalc_controller.rb
+++ app/controllers/teamcalc_controller.rb
@@ -3,7 +3,14 @@ class TeamcalcController < ApplicationController
   end
   def solution
     @num_teams = params[:num_teams]
-    @num_students = params[:num_students]
+    @students = params[:students]
+    @list_students = @students.split(/\r\n|\n/) # create separate entries per line from text area
+    @list_students.reject!(&:empty?) # remove empty values from students array
+    @num_students = @list_students.count # number of students in the array
+    @size_normal = @num_students.to_i / @num_teams.to_i # number of students in normal teams 
+    @num_oddman = @num_students.to_i % @num_teams.to_i # number of oddman teams
+    @size_oddman = @size_normal + 1 # number of students in oddman teams
+    @num_normal = @num_teams.to_i - @num_oddman # number of normal teams
   	# redirect_to "/posts?name=#{@name}", notice: "Good work, #{@name}! We will get back to you shortly."
   end
 end
--- app/helpers/teamcalc_helper.rb
+++ app/helpers/teamcalc_helper.rb
@@ -3,16 +3,16 @@ module TeamcalcHelper
 		a = @num_students.to_i
 		b = @num_teams.to_i
 		size_normal = a / b # number of students in normal size team 
-		d = a % b # number of oddman teams
+		num_oddman = a % b # number of oddman teams
 		size_oddman = size_normal + 1 # number of students in oddman teams
-		num_normal = b - d # number of normal teams
-		num_oddman = d # number of oddman teams
+		num_normal = b - num_oddman # number of normal teams
 
-		if a % b == 0
-			@team_size = a / b
-			return "There will be #{@num_teams} of #{size_normal}"
+		if a % b == 0 && a / b >= 2
+			return "There will be #{num_normal} team(s) of #{size_normal}."
+		elsif a / b < 2
+			return "You have too many teams, which would result in there being 1 or more teams of 1. Input a greater number of students or a smaller number of teams to correct this."
 		else
-			return "There will be #{num_normal} teams of #{size_normal} and #{num_oddman} teams of #{size_oddman}"
+			return "There will be #{num_normal} team(s) of #{size_normal} and #{num_oddman} team(s) of #{size_oddman}."
 		end
-	end
+	end    
 end
